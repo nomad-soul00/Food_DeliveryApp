0f1fcd16e266d9023b9bf481352dcd4146fd5360
Nomad-soul00 <sahilahamed813@gmail.com>
2025-08-26 17:18:59 +0530

    card updated

--- client/src/Screens/Home.jsx
+++ client/src/Screens/Home.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from '../components/Navbar.jsx'
 import Footer from '../components/Footer.jsx'
 import Card from '../components/Card.jsx'
 import Carousel from '../components/Carousel.jsx'
 
 function Home() {
+
+    const [foodItem, setFoodItem] = useState([]);
+    const [foodCategory, setFoodCategory] = useState([]);
+
+    const loadData = async () => {
+        let response = await fetch('http://localhost:5000/api/display/foodData', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        response = await response.json();
+        setFoodItem(response[0]);
+        setFoodCategory(response[1]);
+    }
+
+    useEffect(() => {
+        loadData();
+    }, []);
+
+
+
+
     return (
         <div>
             <div><Navbar /></div>
@@ -12,13 +36,36 @@ function Home() {
                 <Carousel />
             </div>
 
-            <div className='m-3'>
+            <div className='container m-3'>
+                {
+                    (foodCategory.length !== 0) ? foodCategory.map((data, index) => {
+                        return (
+                            <div key={data._id}>
+
+                                <div className='fs-3 m-3' >
+                                    {data.CategoryName}
+                                </div>
+                                <hr />
+                                {
+                                    (foodItem.length !== 0) && foodItem.filter((item) => {
+                                        return item.CategoryName === data.CategoryName
+                                    }).map((filteredItems, index) => {
+                                        return (
+                                            <div key={filteredItems._id}>
+                                                <Card name={filteredItems.name} 
+                                                img={filteredItems.img} />
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                        )
+                    })
+                        : <div>Loading</div>
+                }
+
                 <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                </div>
+            </div>
             <div><Footer /></div>
         </div>
     )
